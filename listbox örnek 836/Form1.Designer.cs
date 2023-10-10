a32c1db7aae87be08d6bc5a67745e8b51ed5e254
ilayda yalçın <146716704+ilaydayacin@users.noreply.github.com>
2023-10-10 15:40:59 +0300

    Add files via upload

---
+++ listbox örnek 836/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace listbox_örnek_836
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lbTek = new System.Windows.Forms.ListBox();
+            this.lbCift = new System.Windows.Forms.ListBox();
+            this.txtSayi = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnEkle.Location = new System.Drawing.Point(334, 42);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(181, 109);
+            this.btnEkle.TabIndex = 0;
+            this.btnEkle.Text = "SAYI EKLE";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(12, 107);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Tek";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(179, 107);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 24);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Çİft";
+            // 
+            // lbTek
+            // 
+            this.lbTek.FormattingEnabled = true;
+            this.lbTek.Location = new System.Drawing.Point(12, 132);
+            this.lbTek.Name = "lbTek";
+            this.lbTek.Size = new System.Drawing.Size(120, 264);
+            this.lbTek.TabIndex = 3;
+            // 
+            // lbCift
+            // 
+            this.lbCift.FormattingEnabled = true;
+            this.lbCift.Location = new System.Drawing.Point(182, 132);
+            this.lbCift.Name = "lbCift";
+            this.lbCift.Size = new System.Drawing.Size(120, 264);
+            this.lbCift.TabIndex = 4;
+            // 
+            // txtSayi
+            // 
+            this.txtSayi.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtSayi.Location = new System.Drawing.Point(16, 42);
+            this.txtSayi.Name = "txtSayi";
+            this.txtSayi.Size = new System.Drawing.Size(286, 26);
+            this.txtSayi.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(547, 450);
+            this.Controls.Add(this.txtSayi);
+            this.Controls.Add(this.lbCift);
+            this.Controls.Add(this.lbTek);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnEkle);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox lbTek;
+        private System.Windows.Forms.ListBox lbCift;
+        private System.Windows.Forms.TextBox txtSayi;
+    }
+}
+
